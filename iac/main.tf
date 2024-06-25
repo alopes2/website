69607f68dc5b4c8b08f41707613ae54808d5b52a
Andre Lopes <andrevitorlopes@gmail.com>
2024-06-25 17:47:08 +0200

    Add custom vercel domain

--- iac/main.tf
+++ iac/main.tf
@@ -8,3 +8,8 @@ resource "vercel_project" "website" {
     repo = "alopes2/website"
   }
 }
+
+resource "vercel_project_domain" "example_redirect" {
+  project_id = vercel_project.website.id
+  domain     = "andrelopes.vercel.app"
+}
