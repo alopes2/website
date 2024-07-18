4dbf9f88c74d83924b32bc3dc9447a83f8cbe3e4
Andre <andrevitorlopes@gmail.com>
2024-07-18 21:43:30 +0200

    Add logo and starting styles

---
+++ src/components/layout/logo.tsx
@@ -0,0 +1,4 @@
+import classes from './logo.module.scss';
+export default function Logo() {
+  return <div className={classes.logo}>Andre Lopes</div>;
+}
---
+++ src/components/layout/navigation.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import Logo from './logo';
+
+import classes from './navigation.module.scss';
+
+export default function Navigation() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <Logo />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
