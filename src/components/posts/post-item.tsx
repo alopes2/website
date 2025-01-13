99efe13bb1b37a40cf241c68efd73fa2de65979b
Andre <andrevitorlopes@gmail.com>
2025-01-13 21:21:45 +0100

    Update a few things

--- src/components/posts/post-item.tsx
+++ src/components/posts/post-item.tsx
@@ -18,17 +18,19 @@ export default function PostItem({ post }: PostItemProps) {
   const linkPath = `/posts/${post.slug}`;
 
   return (
-    <li className={classes.post}>
-      <Link href={linkPath}>
-        <div className={classes.image}>
-          <Image src={imagePath} alt={post.title} width={300} height={200} />
-        </div>
-        <div className={classes.content}>
-          <h3>{post.title}</h3>
-          <time>{date}</time>
-          <p>{post.excerpt}</p>
-        </div>
-      </Link>
-    </li>
+    <>
+      <li className={classes.post}>
+        <Link href={linkPath}>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={post.title} width={200} height={150} />
+          </div>
+          <div className={classes.content}>
+            <h3>{post.title}</h3>
+            <time>{date}</time>
+            <p>{post.excerpt}</p>
+          </div>
+        </Link>
+      </li>
+    </>
   );
 }
