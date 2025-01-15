dde3ed1b7e5de1c0c5268ecb6809ea7ad3f8e855
Andre <andrevitorlopes@gmail.com>
2025-01-15 21:11:01 +0100

    Add current year variable to footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,11 +4,12 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import { LinkedIn } from '@mui/icons-material';
 
 export default function Navigation() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={classes.footer} style={{ flexShrink: 0 }}>
       <Container sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
         <Box sx={{ textAlign: 'left' }}>
-          <p>Copyright © 2025 Andre Lopes</p>
+          <p>Copyright © {currentYear} Andre Lopes</p>
         </Box>
         <Box
           sx={{
@@ -19,7 +20,6 @@ export default function Navigation() {
         >
           <GitHubIcon />
           <LinkedIn />
-          
         </Box>
       </Container>
     </footer>
