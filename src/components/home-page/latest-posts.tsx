a39ff1c9dfc655dfc0a594ce4978ad29c69cbf44
Andre <andrevitorlopes@gmail.com>
2025-02-25 21:22:02 +0100

    Add spinner to latest posts

--- src/components/home-page/latest-posts.tsx
+++ src/components/home-page/latest-posts.tsx
@@ -4,22 +4,23 @@ import PostsGrid from '../posts/posts-grid';
 import classes from './latest-posts.module.scss';
 import { Suspense } from 'react';
 import Container from '@mui/material/Container';
+import LoadingSpinner from '@/components/ui/loading-spinner';
 
-// TODO: Change/outsource this component
+// Separate component for fetching posts
+async function LatestPostsComponent() {
+  const posts = await getLatestPosts();
+  return <PostsGrid posts={posts} />;
+}
+
 export default function LatestPosts() {
   return (
     <section className={classes.latest}>
       <Container>
         <h2>Recent Posts</h2>
-        <Suspense fallback={<p>Fetching posts...</p>}>
+        <Suspense fallback={<LoadingSpinner message="Fetching posts..." />}>
           <LatestPostsComponent />
         </Suspense>
       </Container>
     </section>
   );
-}
-
-async function LatestPostsComponent() {
-  const posts = await getLatestPosts();
-  return <PostsGrid posts={posts} />;
 }
